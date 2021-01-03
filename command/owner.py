6593e9efc484b567259100c6e218015a68cb0f55
chick_0 <chick_0@kakao.com>
2021-01-04 02:38:58 +0900

    help message change

--- command/owner.py
+++ command/owner.py
@@ -3,14 +3,14 @@
 from discord.ext import commands
 
 
-class Command(commands.Cog, name="관리자용 명령어"):
-    @commands.command(help="봇을 종료합니다", hidden=True)
+class Command(commands.Cog, name="봇 주인용 명령어"):
+    @commands.command(help="봇을 종료합니다")
     @commands.is_owner()
     async def close(self, ctx: commands.context):
         await ctx.send(":wave:")
         await ctx.bot.close()
 
-    @commands.command(help="모든 음성 채널에서 나갑니다", hidden=True)
+    @commands.command(help="모든 음성 채널에서 나갑니다")
     @commands.is_owner()
     async def leave_all(self, ctx: commands.context):
         for voice_client in ctx.bot.voice_clients:
