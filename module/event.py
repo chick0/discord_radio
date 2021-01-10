58e98a902b396dbb660656665c9fc397330eccff
chick_0 <chick_0@kakao.com>
2021-01-10 15:13:59 +0900

    쿨타임 오류 메시지 변경

--- module/event.py
+++ module/event.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from random import choice
 from logging import getLogger
 
 from discord import Status, Activity, ActivityType
@@ -50,7 +51,28 @@ async def on_command(ctx):
 async def on_command_error(ctx, error):
     if error.__class__.__name__ == "CommandOnCooldown":
         time = str(error).rsplit(" ", 1)[-1][:-1]
-        await ctx.reply(f"```뭐 그리 급해, {time}초만 기다려```")
+        message = ["고양이가 횡단보도를 건너는 중...",
+                   "너구리가 솜사탕을 헹구는 중...",
+                   "스폰지밥이 게살버거를 뒤집는 중...",
+                   "오븐에서 산딸기 파이가 구워지는 중...",
+                   "오리너구리 페리가 두펀스머츠를 제압하는 중...",
+                   "행복 회로가 불타는 중...",
+                   "돼지 저금통에 동전을 넣는 중...",
+                   "넥서스가 공격받는 중...",
+                   "엘 루비오의 금고가 털리는 중...",
+                   "고양이가 그루밍 하는 중...",
+                   "컴파일하는 척하면서 고양이 쓰다듬는 중...",
+                   "선생님 몰래 춤추는 중...",
+                   "치킨이 튀겨지는 중...",
+                   "고양이가 파쿠르 하는 중...",
+                   "샷건으로 키보드가 부서지는 중...",
+                   "하늘에서 음식이 내리는 중...",
+                   "하울의 성이 움직이는 중...",
+                   "가상머신이 부팅되는 중...",
+                   "좌측담장 넘어가는 중...",
+                   "저궤도 이온 포를 준비하는 중..."]
+
+        await ctx.reply(f"```\n{choice(message)} {time}초만 기다려주세요...\n```")
         return
 
     if error.__class__.__name__ in ["CommandNotFound", "CheckFailure", "NotOwner"]:
