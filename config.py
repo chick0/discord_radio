f22003bd892bf58c43b6c2203fc14eede96f2c71
chick_0 <chick_0@kakao.com>
2021-01-12 00:00:43 +0900

    conf 인코딩 지정

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ from configparser import ConfigParser
 config = ConfigParser()
 for conf_file in listdir(path.join("conf")):
     if conf_file.endswith(".ini"):
-        config.read(path.join("conf", conf_file))
+        config.read(filenames=path.join("conf", conf_file),
+                    encoding="utf-8")
